b53340c6435b1014b1f6a761bc5ca29bb181d890
MrTyoom <112854542+MrTyoom@users.noreply.github.com>
2023-04-09 23:31:56 +0300

    Update buttons.py
    added an effect of "clickable" button

--- buttons.py
+++ buttons.py
@@ -3,9 +3,10 @@ from constants import *
 
 class Button:
     def __init__(self, image, x, y, size):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * size), int(height * size)))
+        self.width, self.height = image.get_width(), image.get_height()
+        self.size = size
+        self.original_image = image
+        self.image = pygame.transform.scale(image, (int(self.width * size), int(self.height * size)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.clicked = False
@@ -15,11 +16,14 @@ class Button:
         pos = pygame.mouse.get_pos()
 
         if self.rect.collidepoint(pos):
+            self.image = pygame.transform.scale(CLICKED_BUTTON[self.original_image], (int(self.width * self.size),
+                                                                                    int(self.height * self.size)))
             if pygame.mouse.get_pressed()[0] and not self.clicked:
                 action = True
                 self.clicked = 1
 
-        if not pygame.mouse.get_pressed()[0]:
+        if not self.rect.collidepoint(pos):
+            self.image = pygame.transform.scale(self.original_image, (int(self.width * self.size), int(self.height * self.size)))
             self.clicked = 0
 
         surface.blit(self.image, (self.rect.x, self.rect.y))

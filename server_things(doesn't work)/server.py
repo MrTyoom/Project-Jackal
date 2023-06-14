069ddfe980769b5a62b333c9035867798f3ed599
MrTyoom <112854542+MrTyoom@users.noreply.github.com>
2023-06-14 22:06:37 +0300

    Create server.py

---
+++ server_things(doesn't work)/server.py
@@ -0,0 +1,44 @@
+import socket
+from _thread import *
+
+server = "my_ip"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("In process")
+
+
+def threaded_client(conn):
+    conn.send(str.encode("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("No data, disconnect")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+while True:
+    connection, address = s.accept()
+    print("Connected to:", address)
+
+    start_new_thread(threaded_client, (connection,))
